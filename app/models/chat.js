86fe168e1ac12eda2da145b777d6420a3ab15abf
Kevin Stewart <kevinjstew@gmail.com>
2015-02-15 20:42:44 -0500

    Created chat fixtures

--- app/models/chat.js
+++ app/models/chat.js
@@ -1,6 +1,36 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  name: DS.attr('string')
+
+var Chat = DS.Model.extend({
+	name: DS.attr('string')
 
 });
+
+
+Chat.reopenClass({
+	FIXTURES: [
+		{
+			"id": 1,
+			"name": "Kevin Stewart"
+		},
+		{
+			"id": 2,
+			"name": "Jerry Seinfeld"
+		},
+		{
+			"id": 3,
+			"name": "Michael Scott"
+		},
+		{
+			"id": 4,
+			"name": "Warren Buffett"
+		},
+		{
+			"id": 5,
+			"name": "Dolan Trump"
+		}
+
+	]
+});
+
+export default Chat;
